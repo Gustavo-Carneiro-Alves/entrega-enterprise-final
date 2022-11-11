d765b408ac3bbbc72509146302e9a9b6195d34c0
Gustavo-Carneiro-Alves <rm88392@fiap.com.br>
2022-11-11 10:23:32 -0300

    tagHelper criado

---
+++ TagHelpers/SuporteTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GS_GreenCycle.TagHelpers
+{
+    public class SuporteTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string email = context.AllAttributes["mailTo"].Value.ToString();
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailTo:" + email);
+            output.Content.SetContent("Suporte");
+        }
+    }
+}
